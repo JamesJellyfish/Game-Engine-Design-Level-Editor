697187735a080a380236e1ca330f0988988c1275
Trey C <treyton.cowell@ontariotechu.net>
2021-12-02 14:04:36 -0500

    UI Enhanced

--- Assets/ScriptsandDLLs/backgroundSound.cs
+++ Assets/ScriptsandDLLs/backgroundSound.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class backgroundSound : MonoBehaviour
 {
-
     public AudioSource audioSource;
-    // Start is called before the first frame update
+
     void Start()
     {
         audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
